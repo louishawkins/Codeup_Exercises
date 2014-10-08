d0dfe79332a7a8035cfad3d8db91ab4e265d038b
Louis Hawkins <louishawkins@gmail.com>
2014-10-08 09:48:42 -0500

    added argument validation, error checking

--- arithmetic.php
+++ arithmetic.php
@@ -1,23 +1,59 @@
 <?php
 
 function add($a, $b) {
-	echo $a + $b . "\n";
+	if (is_numeric($a) && is_numeric($b)) {
+		echo (int) $a + (int) $b . "\n";
+	}
+	else {
+		echo "ERROR: Both arguments must be numbers\n";
+	}
 }
 
 function subtract($a, $b) {
-	echo $a - $b . "\n";
+	if (is_numeric($a) && is_numeric($b)) {
+		echo (int) $a - (int) $b . "\n";
+	}
+	else {
+		echo "ERROR: Both arguments must be numbers\n";
+	}
 }
 
 function multiply($a, $b) {
-	echo $a * $b . "\n";
+	if (is_numeric($a) && is_numeric($b)) {
+		echo (int) $a * (int) $b . "\n";
+	}
+	else {
+		echo "ERROR: Both arguments must be numbers\n";
+	}
 }
 
 function divide($a, $b) {
-	echo $a / $b . "\n";
+	if (is_numeric($a) && is_numeric($b)) {
+		if ($b == 0){
+			echo "ERROR: Divide by 0 -- $a / $b";
+		}
+		else {
+			echo (int) $a / (int) $b . "\n";
+		}
+	}
+	else {
+		echo "ERROR: Both arguments must be numbers\n";
+	}
 }
 function modulus($a, $b) {
-	echo $a % $b . "\n";
+	if (is_numeric($a) && is_numeric($b)) {
+		if ($b != 0) {
+			echo (int) $a % (int) $b . "\n";
+		}
+		else {
+			echo "ERROR: Divide by 0 -- $a % $b";
+		}
+	}
+	else {
+		echo "ERROR: Both arguments must be numbers\n";
+	}
 }
+
 
 echo "100%13 is " . 100 % 13 . " & ";
 modulus(100, 13);
@@ -33,6 +69,14 @@ subtract(8,3);
 
 echo "200 + 30 is " . 200 + 30 . " & ";
 add(200, 30);
+
+echo "\n\nERROR TESTING :: \n\n";
+echo "29 / 0 is ";
+divide(29, 0);
+echo "\n60 % 0 is ";
+modulus(60, 0);
+echo "\n50 + bananas ";
+add(50, "bananas");
 
 exit(0);
 
