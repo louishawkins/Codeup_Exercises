c3cc80bbb3ea3d6c8fd8a92840b5af2110a364e5
Louis Hawkins <louishawkins@gmail.com>
2014-12-05 11:52:31 -0600

    Function checks if string in array.

--- search_arrays.php
+++ search_arrays.php
@@ -1,14 +1,12 @@
 <?php
-// the data -- first names
+
 $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
-// the function
 function findName($name, $names) {
 	return in_array($name, $names) == false ? 'false' : 'true';
 }
 
-// call the function
 echo findName('Tina', $names);
 echo findName('Mike', $compare);
 
